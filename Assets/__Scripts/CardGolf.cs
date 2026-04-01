c2b70df5a4c3830a9265c29e3038612a46d05b8b
nhs50-source <nhs50@scarletmail.rutgers.edu>
2026-03-31 22:00:34 -0400

    Changes to scripts + cards sprites

--- Assets/__Scripts/CardGolf.cs
+++ Assets/__Scripts/CardGolf.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 // This enum defines the variable type eCardState with four named values.      // a
 public enum eCardState { drawpile, mine, target, discard }
 
-public class CardProspector : Card
-{ // Make CardProspector extend Card        // b
-    [Header("Dynamic: CardProspector")]
+public class CardGolf : Card
+{ // Make CardGolf extend Card        // b
+    [Header("Dynamic: CardGolf")]
     public eCardState state = eCardState.drawpile;                   // c
                                                                      // The hiddenBy list stores which other cards will keep this one face down
-    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    public List<CardGolf> hiddenBy = new List<CardGolf>();
     // The layoutID matches this card to the tableau JSON if it’s a tableau card
     public int layoutID;
     // The JsonLayoutSlot class stores information pulled in from JSON_Layout
     public JsonLayoutSlot layoutSlot;
 
     /// <summary>
-    /// Informs the Prospector class that this card has been clicked.
+    /// Informs the Golf class that this card has been clicked.
     /// </summary>
     override public void OnMouseUpAsButton()
     {
         // Uncomment the next line to call the base class version of this method
         // base.OnMouseUpAsButton();                                          // a
-        // Call the CardClicked method on the Prospector Singleton
-        Prospector.CARD_CLICKED(this);
+        // Call the CardClicked method on the Golf Singleton
+        Golf.CARD_CLICKED(this);
         base.OnMouseUpAsButton();// b
     }
     public bool IsBlocked()
